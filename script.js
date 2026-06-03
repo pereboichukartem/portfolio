6978eccf97815f4faf3d444a4fc5f42ab2e3d61b
pereboichukartem <pereboichukartem@gmail.com>
2026-06-03 13:10:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+// --- МОБІЛЬНЕ МЕНЮ (Гамбургер) ---
 const hamburger = document.getElementById('hamburger');
 const navLinks = document.getElementById('nav-links');
 
@@ -16,49 +16,48 @@ if (hamburger && navLinks) {
     });
 }
 
+// --- ФОРМА В ТЕЛЕГРАМ ---
+const tgForm = document.getElementById('tg-form');
 
-const TOKEN = "8893397141:AAG8-tN8GZVy5vmJ-tFUOMbyhLHgf7WlWkU"; 
+if (tgForm) {
+    const TOKEN = "8893397141:AAG8-tN8GZVy5vmJ-tFUOMbyhLHgf7W1wkU"; 
+    const CHAT_ID = "1367650899"; 
+    const URI_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
 
-const CHAT_ID = "1367650899"; 
+    tgForm.addEventListener('submit', function(e) {
+        e.preventDefault();
 
-const URI_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+        let name = document.getElementById('name').value;
+        let email = document.getElementById('email').value;
+        let message = document.getElementById('message').value;
 
-document.getElementById('tg-form').addEventListener('submit', function(e) {
-    e.preventDefault(); 
+        let text = `<b>Нове замовлення з сайту!</b>\n\n`;
+        text += `<b>Ім'я:</b> ${name}\n`;
+        text += `<b>Email:</b> ${email}\n`;
+        text += `<b>Повідомлення:</b> ${message}`;
 
-
-    let name = document.getElementById('name').value;
-    let email = document.getElementById('email').value;
-    let message = document.getElementById('message').value;
-
-    
-    let text = `<b>Нове замовлення з сайту!</b>\n\n`;
-    text += `<b>Ім'я:</b> ${name}\n`;
-    text += `<b>Email:</b> ${email}\n`;
-    text += `<b>Повідомлення:</b> ${message}`;
-
-    
-    fetch(URI_API, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            chat_id: CHAT_ID,
-            text: text,
-            parse_mode: 'html' 
+        fetch(URI_API, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                chat_id: CHAT_ID,
+                text: text,
+                parse_mode: 'html'
+            })
         })
-    })
-    .then(res => {
-        if(res.ok) {
-            alert("Повідомлення успішно відправлено!\nНайближчим часом я вам напишу!"); 
-            this.reset(); 
-        } else {
-            alert("Сталася помилка. Спробуйте пізніше.");
-        }
-    })
-    .catch(err => {
-        console.error(err);
-        alert("Помилка відправки.");
+        .then(res => {
+            if(res.ok) {
+                alert("Повідомлення успішно відправлено!");
+                this.reset();
+            } else {
+                alert("Сталася помилка. Спробуйте пізніше.");
+            }
+        })
+        .catch(err => {
+            console.error(err);
+            alert("Помилка відправки.");
+        });
     });
-});
+}
